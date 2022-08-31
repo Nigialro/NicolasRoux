603a21908d1194f85615624993bf6e8bb99a28b2
Nigialro <112541578+Nigialro@users.noreply.github.com>
2022-08-31 12:02:58 +0200

    Create test.js

---
+++ JS/test.js
@@ -0,0 +1,13 @@
+let numberOfSeasons = 6;
+
+console.log(numberOfSeasons);
+
+let numberOfEpisodes = 12;
+
+console.log(numberOfEpisodes);
+
+let episodeTime = 45;
+
+console.log(episodeTime);
+
+commercialTime
